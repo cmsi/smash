0870d7b08e706ee554a9cfb27113f49fac1ef698
Kazuya Ishimura <ishimura@ims.ac.jp>
2018-05-08 19:19:33 +0900

    Remove subroutine setdefault1

--- src/main.F90
+++ src/main.F90
@@ -153,10 +153,6 @@ end
       type(typebasis),intent(inout) :: databasis
       type(typecomp),intent(inout) :: datacomp
 !
-! Set defaults before reading input file
-!
-      call setdefault1(datacomp)
-!
 ! Read input data and open checkpoint file if necessary
 !
       if(datacomp%master) call opendatfile(datacomp)
@@ -200,78 +196,6 @@ end
 ! Set atom charge including dummy atom
 !
       call setcharge(datamol,datacomp)
-!
-      return
-end
-
-
-!-------------------------
-  subroutine setdefault1(datacomp)
-!-------------------------
-!
-! Set defaults before reading input file
-!
-      use modmolecule, only : multi, charge
-      use modtype, only : typecomp
-      implicit none
-      type(typecomp),intent(inout) :: datacomp
-!
-!     datacomp%nwarn= 0
-!     datacomp%memmax = 1000000000
-!     datacomp%memused= 0
-!     datacomp%memusedmax= 0
-!     memory = ''
-!     maxiter= 150
-!     maxdiis= 20
-!     maxsoscf= 20
-!     maxqc   = 15
-!     maxqcdiag= 100
-!     maxqcdiagsub= 10
-!     fdiff  =.true.
-!     scfconv='DIIS'
-!     extrap =.false.
-!     threshsoscf= 0.25D+00
-!     threshqc   = 1.0D-05
-!     threshover = 1.0D-06
-!     threshatom = 2.0D-01
-!     threshdiis = 6.0D-01
-!     threshmp2cphf=1.0D-10
-!     idftex = 0
-!     idftcor= 0
-!     iprint = 2
-!     bohr   =.false.
-!     spher  =.true.
-!     spher_g=.true.
-!     nopt   = 100
-!     optconv= 1.0D-04
-!     cartesian=.false.
-      multi  = 1
-      charge = 0.0D+00
-!     bqrad(:)=1.0D+00
-!     nvfz= 0
-!     maxmp2diis= 20
-!     maxmp2iter= 100
-!
-!     cutint2=-1.0d+00
-!     nrad = 0
-!     nleb = 0
-!     ncore= -1
-!     dconv=-1.0D+00
-!     threshweight=-1.0D+00
-!     threshrho=-1.0D+00
-!     threshdfock=-1.0D+00
-!     threshdftao=-1.0D+00
-!
-!     precision='MEDIUM'
-!     flagecp= .false.
-!     scftype='RHF'
-!     method='HARTREE-FOCK'
-!     runtype='ENERGY'
-!     basis='STO-3G'
-!     guess='HUCKEL'
-!     ecp=''
-!     check=''
-!     octupole=.false.
 !
       return
 end
